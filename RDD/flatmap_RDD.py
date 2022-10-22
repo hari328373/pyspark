eefb45f42ec4995d59ad1d304d84c389745dfa85
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-22 11:09:37 +0530

    flatmap in RDD

---
+++ RDD/flatmap_RDD.py
@@ -0,0 +1,6 @@
+from pyspark import SparkContext
+sc=SparkContext.getOrCreate()
+rdd1=sc.parallelize([1,2,3,4,5,6])
+rdd_flatmap=rdd1.flatMap(lambda x : (x, x**2,100*x)) # return a sequence rather than a singled item
+print("rdd1 : ",rdd1.collect())
+print("flat_map :",rdd_flatmap.collect())
