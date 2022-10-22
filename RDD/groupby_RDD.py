25423ea4f9b925585b1fc836c820bec8fb3efeee
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-22 11:10:02 +0530

    groupby in RDD

---
+++ RDD/groupby_RDD.py
@@ -0,0 +1,7 @@
+from pyspark import SparkContext
+sc = SparkContext.getOrCreate()
+rdd = sc.parallelize([1,2,6,5,3,5,8,9])
+result = rdd.groupBy(lambda x:x%2).collect()
+for (x,y) in result:
+    print([(x,sorted(y))])
+
