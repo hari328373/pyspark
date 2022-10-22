aea217ea58229e7811550c924c8eb8f1a6916dbe
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-22 11:08:56 +0530

    filter the elements in RDD

---
+++ RDD/filter_RDD.py
@@ -0,0 +1,18 @@
+
+from pyspark import SparkContext
+sc=SparkContext.getOrCreate()
+
+def statswith(w):
+    return w.startswith("s")
+
+#filter
+my_coll ="string is a data structure in python".split(' ')
+words = sc.parallelize(my_coll)
+filterwords = words.filter(lambda w:statswith(w))
+print(filterwords.collect())
+
+# filter
+x_rdd = sc.parallelize([1,2,3,4,5])
+y_rdd= x_rdd.filter(lambda x:x%2==0)
+print(x_rdd.collect())
+print(y_rdd.collect())
