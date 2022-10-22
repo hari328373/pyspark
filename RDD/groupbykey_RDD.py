2b7d361ed1695085b6c1ea019921b78dfa5f9882
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-22 11:10:15 +0530

    groupbykey in RDD

---
+++ RDD/groupbykey_RDD.py
@@ -0,0 +1,14 @@
+from pyspark import SparkContext
+sc = SparkContext.getOrCreate()
+
+# groupbykey
+rdd_1 = sc.parallelize([(1,2),(1,3),(1,5),(2,5),(3,5),(5,8),(9,5)])
+
+rdd_2 = rdd_1.groupByKey()
+# print([(j[0],[i for i in j[1]]) for j in rdd_2.collect()])
+for i in rdd_2.collect():
+    for j in i[1]:
+        print([i[0],j])
+rdd_3 = rdd_1.groupByKey()
+for (k,v) in rdd_3.collect():
+    print((k,list(v)))
