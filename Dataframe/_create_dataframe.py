07902ffb8f725c0d13739cd9588e71ca5f53d9d3
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-22 10:58:48 +0530

    create the Dataframe

---
+++ Dataframe/_create_dataframe.py
@@ -0,0 +1,7 @@
+# create dataframe
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local").appName("demo").getOrCreate()
+data = ('1','ravi','101'),('2','sam','102'),('3','hari','103')
+columns = ['id','name','rollnum']
+df = spark.createDataFrame(data=data,schema=columns)
+df.show()
