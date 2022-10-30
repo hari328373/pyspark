09b6e24e204108c2b67d8d0eb502018e25bf720f
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-30 21:23:37 +0530

    reduce Action

--- RDD/reduce_RDD.py
+++ RDD/reduce_RDD.py
@@ -7,5 +7,5 @@ print("sum of num :",sc.parallelize(range(1,21)).reduce(lambda x,y:x+y))
 # reducebykey-->Transformation
 
 rdd_1 = sc.parallelize([(1,2),(1,3),(1,5),(2,5),(3,5),(5,8),(9,5)])
-rdd_4 = rdd_1.reduceByKey(lambda x,y : x+y).collect()
+rdd_4 = rdd_1.reduceByKey(lambda x,y : x*y).collect()
 print(rdd_4)
