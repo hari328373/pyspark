3ccfc2c81926aada6324fe184fc6b3520dd4db87
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-30 21:21:46 +0530

    map narrow transformation

--- RDD/map_RDD.py
+++ RDD/map_RDD.py
@@ -1,12 +1,10 @@
 # transformation
 
-import findspark
-findspark.init()
 from pyspark import SparkContext
 sc = SparkContext.getOrCreate()
 
 data=([1,2,3,4,5])
 rdd = sc.parallelize(data)
 rdd_map=rdd.map(lambda x :(x,x**2)) # return the new RDD formed with given functionality
-print("rdd : ",rdd.take(3))
+print("rdd : ",rdd.take(3)) # return mention num of elements
 print("rdd_map :",rdd_map.collect())
