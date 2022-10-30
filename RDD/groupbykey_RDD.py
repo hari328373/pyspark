8ecfb0fc76393a02ae787057bdcb45e523e38a3e
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-30 21:21:27 +0530

    groupbykey wide transformation

--- RDD/groupbykey_RDD.py
+++ RDD/groupbykey_RDD.py
@@ -1,3 +1,5 @@
+# wide transformation
+
 from pyspark import SparkContext
 sc = SparkContext.getOrCreate()
 
@@ -5,10 +7,8 @@ sc = SparkContext.getOrCreate()
 rdd_1 = sc.parallelize([(1,2),(1,3),(1,5),(2,5),(3,5),(5,8),(9,5)])
 
 rdd_2 = rdd_1.groupByKey()
-# print([(j[0],[i for i in j[1]]) for j in rdd_2.collect()])
-for i in rdd_2.collect():
-    for j in i[1]:
-        print([i[0],j])
-rdd_3 = rdd_1.groupByKey()
+print([(j[0],[i for i in j[1]]) for j in rdd_2.collect()])
+
+'''rdd_3 = rdd_1.groupByKey()
 for (k,v) in rdd_3.collect():
-    print((k,list(v)))
+    print((k,list(v)))'''
