6861951b4653743428b983cd14573b83d7b75cae
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-30 21:18:12 +0530

    create dataframe

--- Dataframe/_create_dataframe.py
+++ Dataframe/_create_dataframe.py
@@ -1,7 +1,13 @@
 # create dataframe
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local").appName("demo").getOrCreate()
-data = ('1','ravi','101'),('2','sam','102'),('3','hari','103')
+data = (1,'ravi',101),(2,'sam',102),(3,'hari',103)
 columns = ['id','name','rollnum']
 df = spark.createDataFrame(data=data,schema=columns)
 df.show()
+
+
+# textfile dataframe
+df1=spark.read.text(r"C:\Users\HARI\OneDrive\Desktop\textfile.txt")
+df1.show(truncate=False)
+df1.printSchema()
