c6a1feeb1a0689e8555337b4409d97b4702d1309
Marripudi Haritha Sumanjali <harimarripudi4@gmail.com>
2022-10-30 21:23:03 +0530

    mapparitions transformation

--- RDD/mapparitions.py
+++ RDD/mapparitions.py
@@ -1,17 +1,23 @@
 # mapparitions
+
+
 from pyspark import SparkContext
 sc = SparkContext.getOrCreate()
 
 
 x = sc.parallelize([1,1,2,3,4,5,6,7,8,9],2)
-print(x.collect())
-print(x.glom().collect()) # glom()--> flattens the elements on the same partition
+print("x:",x.collect())
+print("x glom:",x.glom().collect()) # glom()--> flattens the elements on the same partition
 def f(iter):yield sum(iter)
 y = x.mapPartitions(f)
-print(y.collect())
-print(y.glom().collect())
+print("y:",y.collect())
+print("y glom:",y.glom().collect())
+
+
+
 
 # mappartitionwithindex
+
 def f1(parindex,iter) : yield (parindex,sum(iter))
 z = x.mapPartitionsWithIndex(f1)
-print(z.glom().collect())
+print("z glom :",z.glom().collect())
